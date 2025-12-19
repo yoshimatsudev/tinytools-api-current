9b0cdcf39ccf289c5b9313ce06ab584c60de3e03
Daniel Yoshimatsu <danielyoshipele@gmail.com>
2025-12-19 13:54:53 -0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ export const constants = {
   SCRAPED_BASE_URL: 'https://erp.tiny.com.br/',
   SCRAPED_INVOICE_ENDPOINT: 'services/notas.fiscais.server.php',
   SCRAPED_LOGIN_ENDPOINT: 'services/reforma.sistema.server.php',
-  SCRAPED_FRONT_VERSTION: '3.82.76',
+  SCRAPED_FRONT_VERSTION: '3.83.04',
   AUTH_ERROR_PREFIX: 'alert(',
   INVOICE_ITEM_PREFIX: 'setarArrayItens(',
   TEMP_ITEM_PREFIX: 'callbackEditarItem(',
